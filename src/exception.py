41971f04956853cbe4b071e8556bf2c1729db7f2
Ayush-Bisht001 <bishtayush409@gmail.com>
2026-04-15 16:21:30 +0530

    Project structure with logging and exception handling

---
+++ src/exception.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_number, str(error)
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        self.error_message = error_message_detail(error, error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.error("Divide by Zero error.")
+        raise CustomException(e, sys)
